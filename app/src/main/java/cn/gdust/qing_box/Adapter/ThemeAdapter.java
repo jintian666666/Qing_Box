6acb8ef68639dab5a863095acc4751332a38f946
ljt <jintian666666@gmail.com>
2022-11-25 16:38:24 +0800

    主题管理-UI;无逻辑

--- app/src/main/java/cn/gdust/qing_box/Adapter/ThemeAdapter.java
+++ app/src/main/java/cn/gdust/qing_box/Adapter/ThemeAdapter.java
@@ -3,6 +3,7 @@ package cn.gdust.qing_box.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +20,21 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
 
 //    private Context context;
     private List<Theme> data;
+    private boolean isClick; //是否点击
+    private int currentPosition; //当前项
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public boolean isClick() {
+        return isClick;
+    }
+
+    public void setCurrentPosition(int currentPosition , boolean isClick) {
+        this.currentPosition = currentPosition;
+        this.isClick = isClick;
+    }
 
     public ThemeAdapter(List<Theme> data) {
         this.data = data;
@@ -30,7 +46,19 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
         //创建一个view实例，把子项布局作为参数传进去，然后再把这个view传给ViewHolder的构造函数，最后返回这个viewHolder
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_theme,parent,false);
         final ViewHolder viewHolder = new ViewHolder(view);
+        ItemClickEvent(viewHolder);
         return viewHolder;
+    }
+
+    private void ItemClickEvent(ViewHolder viewHolder) {
+        viewHolder.itemView.setOnClickListener(view -> {
+            if (!isClick){
+                setCurrentPosition(viewHolder.getBindingAdapterPosition(), true);
+            } else {
+                setCurrentPosition(viewHolder.getBindingAdapterPosition(), getCurrentPosition() != viewHolder.getBindingAdapterPosition());
+            }
+            notifyDataSetChanged();
+        });
     }
 
     @Override
@@ -39,6 +67,13 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
         Theme theme = data.get(position);
         holder.textView.setText(theme.getText());
         holder.cardView.setCardBackgroundColor(theme.getColor());
+        if (getCurrentPosition() == position && isClick()){
+            holder.iv_done.setVisibility(View.VISIBLE);
+        }else if (getCurrentPosition() == holder.getBindingAdapterPosition()){
+            holder.iv_done.setVisibility(View.VISIBLE);
+        } else {
+            holder.iv_done.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
@@ -50,12 +85,20 @@ public class ThemeAdapter extends RecyclerView.Adapter<ThemeAdapter.ViewHolder> 
         //定义RecyclerView子项布局中需要变更的元素
         TextView textView;
         MaterialCardView cardView;
+        ImageView iv_done;
 
         public ViewHolder(@NonNull View view) {
             super(view);
             //通过findViewById()获取控件实例
             textView = view.findViewById(R.id.tv_color);
             cardView = view.findViewById(R.id.cardview);
+            iv_done = view.findViewById(R.id.iv_done);
+//            view.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    iv_done.setVisibility(View.VISIBLE);
+//                }
+//            });
         }
     }
 
